64486e2934710023375db17c9ab1f5d243dea871
haris97m <haris00mubaroq@gmail.com>
2023-05-04 12:48:48 +0700

    modified book_controller

--- controller/book_controller.go
+++ controller/book_controller.go
@@ -4,7 +4,7 @@ import (
 	"github.com/haris97m/bookstore-management-system/model"
 )
 
-type BookHandler interface {
+type Bookstore interface {
 	AddBook(book model.Book)
 	GetBookByID(ID int) model.Book
 	RemoveBookByID(ID int)
